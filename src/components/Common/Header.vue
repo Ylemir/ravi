bd95cf46be61b0ab8ad54f5cd6e11d9094b372db
Ylem <ylem.he@139.com>
2026-05-20 23:22:51 +0800

    feat: add theme switching and language selection options in Header component with localization support

--- src/components/Common/Header.vue
+++ src/components/Common/Header.vue
@@ -3,71 +3,71 @@ import type { LangEnum } from '~/enum/appEnum'
 import { language, theme } from '~/store/projectSetting'
 import { availableLocales as locales, loadLanguageAsync } from '~/modules/i18n'
 
-const { t, locale } = useI18n()
-
-const langOptions = locales.map((lang) => {
-  return {
-    label: t(`language.${lang}`),
-    key: lang,
-    disabled: false,
-  }
-})
+const { t } = useI18n()
 
 const languageList = computed(() =>
-  langOptions.map(lang => ({
-    ...lang,
-    disabled: lang.key === language.value,
+  locales.map(lang => ({
+    label: t(`language.${lang}`),
+    key: lang,
+    disabled: lang === language.value,
   }))
 )
 
 async function changeLanguage(key: LangEnum) {
   language.value = key
   await loadLanguageAsync(key)
-  locale.value = key
 }
 
 function changeTheme() {
   theme.value = theme.value ? '' : 'dark'
 }
 
-function hello() {
+function greetUser() {
   window.$message.info(t('Hello', { now: new Date().toLocaleString() }))
 }
 </script>
 
 <template>
-  <n-layout-header pb-1>
+  <n-layout-header pb-1 role="banner">
     <n-space align="center" justify="space-around" size="large">
       <n-space text-2xl align="center" justify="space-around">
-        <router-link to="/">
+        <router-link to="/" aria-label="Home">
           <h1 class="color-#18a058 hover:color-#36ad6a">
             Ravi
           </h1>
         </router-link>
       </n-space>
 
-      <n-space text-4 font-bold justify="space-around">
+      <nav aria-label="Main navigation">
+        <n-space text-4 font-bold justify="space-around">
         <router-link to="/Keyboard" theme-color>
-          {{ t("header.keyboard-mode") }}
-        </router-link>
-        <n-divider vertical />
-        <router-link to="/Search" theme-color>
-          {{ t("header.search-mode") }}
-        </router-link>
-      </n-space>
+            {{ t("header.keyboard-mode") }}
+          </router-link>
+          <n-divider vertical aria-hidden="true" />
+          <router-link to="/Search" theme-color>
+            {{ t("header.search-mode") }}
+          </router-link>
+        </n-space>
+      </nav>
 
-      <n-space text-xl font-medium align="center" justify="space-around">
-        <div i-carbon-user cursor-pointer theme-color @click="hello" />
-        <router-link to="/Settings">
+      <n-space text-xl font-medium align="center" justify="space-around" role="toolbar" aria-label="User actions">
+        <div
+          i-carbon-user cursor-pointer theme-color
+          role="button"
+          tabindex="0"
+          aria-label="Greeting"
+          @click="greetUser"
+        />
+        <router-link to="/Settings" :title="t('header.setting')">
           <div theme-color i="carbon-settings" />
         </router-link>
         <n-dropdown trigger="hover" :options="languageList" @select="changeLanguage">
-          <div i-carbon:ibm-watson-language-translator theme-color />
+          <div i-carbon:ibm-watson-language-translator theme-color :title="t('header.switch-language')"/>
         </n-dropdown>
 
         <n-text cursor-pointer @click="changeTheme">
-          <div v-if="theme" i-carbon-moon theme-color />
-          <div v-else theme-color i="carbon-sun" />
+          <div v-if="theme" i-carbon-moon theme-color :title="t('header.switch-to-light')"/>
+          <div v-else theme-color i="carbon-sun" :title="t('header.switch-to-dark')"/>
         </n-text>
       </n-space>
     </n-space>
